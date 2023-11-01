ea703f38bfad30a2fc7488102dcde7ba95220903
chami98 <chamikara98@gmail.com>
2023-11-01 12:39:49 +0530

    added stack

--- dataStructures.js
+++ dataStructures.js
@@ -1,16 +1,18 @@
-const fruits = ['apple', 'banana', 'cherry'];
+const fruits = ["apple", "banana", "cherry"];
 
 const person = {
-    name: 'John',
-    age: 30,
-    isStudent: false
-  };
+  name: "John",
+  age: 30,
+  isStudent: false,
+};
 
-  const uniqueNumbers = new Set([1, 2, 2, 3, 4, 4]);
+const uniqueNumbers = new Set([1, 2, 2, 3, 4, 4]);
 
-  const userMap = new Map();
-userMap.set('username', 'johndoe');
-userMap.set('email', 'john@example.com');
+const userMap = new Map();
+userMap.set("username", "johndoe");
+userMap.set("email", "john@example.com");
 
-
-  
+const stack = [];
+stack.push("item1");
+stack.push("item2");
+const poppedItem = stack.pop(); // 'item2'
