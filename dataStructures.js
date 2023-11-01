d1eaa98ce1c6f0658d7cbed30f44768689f3acfd
chami98 <chamikara98@gmail.com>
2023-11-01 12:39:14 +0530

    Added maps

--- dataStructures.js
+++ dataStructures.js
@@ -8,4 +8,9 @@ const person = {
 
   const uniqueNumbers = new Set([1, 2, 2, 3, 4, 4]);
 
+  const userMap = new Map();
+userMap.set('username', 'johndoe');
+userMap.set('email', 'john@example.com');
+
+
   
