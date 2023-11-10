4de33c3a8962a802096581be3c0a7ecf8be02387
chami98 <chamikara98@gmail.com>
2023-11-10 22:23:39 +0530

    binary search

---
+++ binarysearch.cs
@@ -0,0 +1,26 @@
+public static int BinarySearch(int[] arr, int target)
+{
+    int left = 0;
+    int right = arr.Length - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+
+        if (arr[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+
+    return -1; // return -1 if the target is not found
+}
