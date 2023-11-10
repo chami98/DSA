da6d4c82b3e3c4b2cb43dbd3c24d54690df9d496
chami98 <chamikara98@gmail.com>
2023-11-10 22:14:04 +0530

    insertion sort

---
+++ insertionsort.cs
@@ -0,0 +1,15 @@
+public static void InsertionSort(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
