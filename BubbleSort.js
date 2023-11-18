ca4fff3a80c00ea741cee1650cd2b6d6699c4ded
chami98 <chamikara98@gmail.com>
2023-11-18 23:58:35 +0530

    bubble sort

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,23 +1,19 @@
 function bubbleSort(arr) {
-    const n = arr.length;
-    let swapped;
-  
-    do {
-      swapped = false;
-      for (let i = 0; i < n - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-          // Swap arr[i] and arr[i+1]
-          [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-          swapped = true;
-        }
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap arr[j] and arr[j+1]
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
-    } while (swapped);
-  
-    return arr;
+    }
   }
-  
-  // Example usage:
-  const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
-  const sortedArray = bubbleSort(unsortedArray);
-  console.log(sortedArray); // [11, 12, 22, 25, 34, 64, 90]
-  
+  return arr;
+}
+
+// Example usage:
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = bubbleSort(unsortedArray);
+console.log(sortedArray); // [11, 12, 22, 25, 34, 64, 90]
