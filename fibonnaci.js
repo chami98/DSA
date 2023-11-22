71725fd6eedce5ae07e4d66582024fa37bde47c0
chami98 <chamikara98@gmail.com>
2023-11-22 23:55:45 +0530

    Fibonacci

--- fibonnaci.js
+++ fibonnaci.js
@@ -1,44 +1,20 @@
-function fibonacciRecursive(n) {
-    if (n <= 0) {
-      return [];
-    } else if (n === 1) {
-      return [0];
-    } else if (n === 2) {
-      return [0, 1];
-    } else {
-      const fib = fibonacciRecursive(n - 1);
-      fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
-      return fib;
+function fibonacciDynamic(n) {
+  if (n <= 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
     }
+    return fib;
   }
-  
-  // Example usage:
-  const n = 10;
-  const fibonacciSeries = fibonacciRecursive(n);
-  console.log(fibonacciSeries); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+}
 
-
-  //Iterative approach
-
-  function fibonacciIterative(n) {
-    if (n <= 0) {
-      return [];
-    } else if (n === 1) {
-      return [0];
-    } else if (n === 2) {
-      return [0, 1];
-    } else {
-      const fib = [0, 1];
-      for (let i = 2; i < n; i++) {
-        fib.push(fib[i - 1] + fib[i - 2]);
-      }
-      return fib;
-    }
-  }
-  
-  // Example usage:
-  const n2 = 10;
-  const fibonacciSeries2 = fibonacciIterative(n2);
-  console.log(fibonacciSeries2); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-  
-  
+// Example usage:
+const n = 10;
+const fibonacciSeries = fibonacciDynamic(n);
+console.log(fibonacciSeries); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
