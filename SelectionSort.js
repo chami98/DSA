9ceb03e70a992e98741822cb6da716a88cd356f9
chami98 <chamikara98@gmail.com>
2023-11-24 23:21:56 +0530

    Selection sort

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,27 +1,36 @@
+// Function to perform selection sort on an array
 function selectionSort(arr) {
-    const n = arr.length;
-  
-    for (let i = 0; i < n - 1; i++) {
-      // Assume the current element is the minimum
-      let minIndex = i;
-  
-      // Find the index of the minimum element in the unsorted part of the array
-      for (let j = i + 1; j < n; j++) {
-        if (arr[j] < arr[minIndex]) {
-          minIndex = j;
-        }
-      }
-  
-      // Swap the found minimum element with the first element
-      if (minIndex !== i) {
-        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+  // Get the length of the array
+  const n = arr.length;
+
+  // Loop over each element of the array
+  for (let i = 0; i < n - 1; i++) {
+    // Assume the current element is the minimum
+    let minIndex = i;
+
+    // Find the index of the minimum element in the unsorted part of the array
+    for (let j = i + 1; j < n; j++) {
+      // If the current element is less than the current minimum, update the minimum
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
       }
     }
-  
-    return arr;
+
+    // If the minimum element is not the current element, swap them
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
   }
-  
-  const unsortedArray = [64, 25, 12, 22, 11];
-  const sortedArray = selectionSort(unsortedArray);
-  console.log(sortedArray); // [11, 12, 22, 25, 64]
-  
+
+  // Return the sorted array
+  return arr;
+}
+
+// Define an unsorted array
+const unsortedArray = [64, 25, 12, 22, 11];
+
+// Sort the array using the selection sort algorithm
+const sortedArray = selectionSort(unsortedArray);
+
+// Log the sorted array to the console
+console.log(sortedArray); // [11, 12, 22, 25, 64]
