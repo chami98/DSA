06880700d06db8b3fa69c4b5078925d9aceaad6f
chami98 <chamikara98@gmail.com>
2023-12-12 23:41:42 +0530

    Added comments

--- binarysearch.cs
+++ binarysearch.cs
@@ -1,26 +1,33 @@
 public static int BinarySearch(int[] arr, int target)
 {
+    // Define the initial left and right boundaries of the search
     int left = 0;
     int right = arr.Length - 1;
 
+    // While the search space is not empty
     while (left <= right)
     {
+        // Calculate the middle index of the search space
         int mid = left + (right - left) / 2;
 
+        // If the target is found, return its index
         if (arr[mid] == target)
         {
             return mid;
         }
 
+        // If the target is greater than the middle element, discard the left half of the search space
         if (arr[mid] < target)
         {
             left = mid + 1;
         }
+        // If the target is less than the middle element, discard the right half of the search space
         else
         {
             right = mid - 1;
         }
     }
 
-    return -1; // return -1 if the target is not found
+    // If the target is not found, return -1
+    return -1; 
 }
