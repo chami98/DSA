fad99eed7fd6702b1619f55384c6c24d63d80fc9
chami98 <chamikara98@gmail.com>
2023-12-12 23:33:18 +0530

    Resolved Conflicts

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,19 +1,4 @@
-#include <iostream>
 #include <vector>
-
-using namespace std;
-
-int binarySearch(vector<int>& arr, int target) {
-    int left = 0;
-    int right = arr.size() - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            left = mid + 1;#include <iostream>
-#include <vector>
-
 using namespace std;
 
 // Function to perform binary search on a sorted array
--- quicksort.py
+++ quicksort.py
@@ -1,23 +1,33 @@
+# This function performs a binary search on a sorted array.
 def binary_search(arr, low, high, x):
+    # If the high index is greater or equal to the low index, continue the search.
     if high >= low:
+        # Calculate the middle index.
         mid = (high + low) // 2
+        # If the middle element is the target, return its index.
         if arr[mid] == x:
             return mid
+        # If the middle element is greater than the target, search in the left half.
         elif arr[mid] > x:
             return binary_search(arr, low, mid - 1, x)
+        # If the middle element is less than the target, search in the right half.
         else:
             return binary_search(arr, mid + 1, high, x)
+    # If the high index is less than the low index, the target is not in the array.
     else:
         return -1
 
-# Test array
+# Define a test array.
 arr = [2, 3, 4, 10, 40]
+# Define the target element.
 x = 10
 
-# Function call
+# Call the binary search function with the test array and target element.
 result = binary_search(arr, 0, len(arr)-1, x)
 
+# If the result is not -1, the target is in the array.
 if result != -1:
     print("Element is present at index", str(result))
+# If the result is -1, the target is not in the array.
 else:
     print("Element is not present in array")
