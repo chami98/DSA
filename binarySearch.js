f6996f516d75e9314c4c1576eaf50ae580398b48
ChamikaraM <ChamikaraM@99x.io>
2024-01-03 23:33:13 +0530

    Add comments

--- binarySearch.js
+++ binarySearch.js
@@ -1,18 +1,28 @@
+// This function performs a binary search on a sorted array
 function binarySearch(arr, target) {
+    // Define the initial left and right boundaries of the search
     let left = 0;
     let right = arr.length - 1;
 
+    // While the search space is not empty
     while (left <= right) {
+        // Calculate the middle index of the search space
         let mid = Math.floor((left + right) / 2);
 
+        // If the target is found, return its index
         if (arr[mid] === target) {
             return mid;
-        } else if (arr[mid] < target) {
+        } 
+        // If the target is greater than the middle element, discard the left half of the search space
+        else if (arr[mid] < target) {
             left = mid + 1;
-        } else {
+        } 
+        // If the target is less than the middle element, discard the right half of the search space
+        else {
             right = mid - 1;
         }
     }
 
+    // If the target is not found, return -1
     return -1;
 }
